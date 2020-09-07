50ad694763c5f380388b59c1edac442843323df3
Roman Seropyan <rseropyan.public@gmail.com>
2020-09-08 00:07:34 +0300

    GetMapping and DeleteMapping basic functionality completed

--- src/main/java/com/space/controller/CosmoportRestController.java
+++ src/main/java/com/space/controller/CosmoportRestController.java
@@ -1,13 +1,14 @@
 package com.space.controller;
 
 import com.space.model.Ship;
+import com.space.model.ShipNotFoundException;
+import com.space.model.ShipType;
 import com.space.service.ShipService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,14 +22,83 @@ public class CosmoportRestController {
         this.shipService = shipService;
     }
 
-    @GetMapping("ships")
-    public ResponseEntity getAllShips() {
+    @GetMapping("/ships")
+    public ResponseEntity<List<Ship>> getAllShips(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String planet,
+            @RequestParam(required = false) ShipType shipType,
+            @RequestParam(required = false) Long after,
+            @RequestParam(required = false) Long before,
+            @RequestParam(required = false, defaultValue = "false") Boolean isUsed,
+            @RequestParam(required = false) Double minSpeed,
+            @RequestParam(required = false) Double maxSpeed,
+            @RequestParam(required = false) Integer minCrewSize,
+            @RequestParam(required = false) Integer maxCrewSize,
+            @RequestParam(required = false) Double minRating,
+            @RequestParam(required = false) Double maxRating,
+            @RequestParam(required = false) ShipOrder order,
+            @RequestParam(required = false, defaultValue = "0") Integer pageNumber,
+            @RequestParam(required = false, defaultValue = "3") Integer pageSize
+            ) {
+
+        List<Ship> ships = shipService.getAllShips();
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=UTF-8");
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        List<Ship> ships = shipService.getAllShips();
+        headers.add("Cache-Control", "no-store");
+
         return new ResponseEntity<>(ships, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/ships/count")
+    public ResponseEntity<Integer> getNumberOfShips(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String planet,
+            @RequestParam(required = false) ShipType shipType,
+            @RequestParam(required = false) Long after,
+            @RequestParam(required = false) Long before,
+            @RequestParam(required = false, defaultValue = "false") Boolean isUsed,
+            @RequestParam(required = false) Double minSpeed,
+            @RequestParam(required = false) Double maxSpeed,
+            @RequestParam(required = false) Integer minCrewSize,
+            @RequestParam(required = false) Integer maxCrewSize,
+            @RequestParam(required = false) Double minRating,
+            @RequestParam(required = false) Double maxRating
+    ) {
+
+        List<Ship> ships = shipService.getAllShips();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=UTF-8");
+        headers.add("Cache-Control", "no-store");
+
+        return new ResponseEntity<>(ships.size(), headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/ships/{id}")
+    public ResponseEntity<Ship> getShipById(@PathVariable("id") Long id) {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            Ship ship = shipService.getShipById(id);
+            headers.add("Cache-Control", "no-store");
+            headers.add("Content-Type", "application/json; charset=UTF-8");
+            return new ResponseEntity<>(ship, headers, HttpStatus.OK);
+        } catch (ShipNotFoundException e) {
+            return new ResponseEntity<>(headers, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/ships/{id}")
+    public ResponseEntity<Ship> deleteShipById(@PathVariable("id") Long id) {
+
+        try {
+            shipService.deleteShipById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ShipNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/com/space/service/ShipService.java
+++ src/main/java/com/space/service/ShipService.java
@@ -1,10 +1,12 @@
 package com.space.service;
 
 import com.space.model.Ship;
+import com.space.model.ShipNotFoundException;
 import com.space.repository.ShipRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ShipService {
@@ -19,4 +21,18 @@ public class ShipService {
         return shipRepository.findAll();
     }
 
+    public Ship getShipById(Long id) {
+        Optional<Ship> ship = shipRepository.findById(id);
+        if (ship.isPresent()) {
+            return ship.get();
+        }
+        throw new ShipNotFoundException();
+    }
+
+    public void deleteShipById(Long id) {
+        if(id != null && shipRepository.existsById(id)) {
+            shipRepository.deleteById(id);
+        }
+        throw new ShipNotFoundException();
+    }
 }
