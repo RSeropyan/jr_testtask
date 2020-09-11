8005be0020e93cb0ec2485e2dee7a0f60a1c216a
Roman Seropyan <rseropyan.public@gmail.com>
2020-09-12 00:45:13 +0300

    PostMapping functionality (including input data validation) completed

---
+++ src/main/java/com/space/service/validators/ShipValidator.java
@@ -0,0 +1,11 @@
+package com.space.service.validators;
+
+import org.springframework.util.MultiValueMap;
+
+public class ShipPropertiesValidator {
+
+    public static boolean isValid(MultiValueMap<String, String> shipProperties) {
+        return true;
+    }
+
+}
